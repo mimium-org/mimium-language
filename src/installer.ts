b2456c93c89ce58fd1d835faa8a4f7ea26f2affc
松浦 知也 Matsuura Tomoya <me@matsuuratomoya.com>
2024-12-10 16:24:57 +0900

    replaced download to npm-downloader-helpr and got to fetch

--- src/installer.ts
+++ src/installer.ts
@@ -4,13 +4,11 @@
 import * as vscode from "vscode";
 
 // node
-import { env } from "process";
 import { platform } from "os";
 import * as fs from "fs";
 import * as path from "path";
-import { spawnSync, spawn } from "child_process";
-import download from "download";
-import got from "got";
+import { spawnSync } from "child_process";
+import { DownloaderHelper, ErrorStats } from "node-downloader-helper";
 import { getMimiumPath, getExecutableCommand } from "./mimium_env";
 import { Version, parseVersion } from "./utils";
 
@@ -19,11 +17,7 @@ const getConfig = () => vscode.workspace.getConfiguration("mimium");
 const getLatestVersionOfMimium = async (): Promise<Version> => {
   const endpoint = `https://api.github.com/repos/tomoyanonymous/mimium-rs/releases`;
   // const endpoint = `https://api.github.com/repos/tomoyanonymous/mimium-rs/releases?client_id=${client_id}?client_secret=${client_secret}`;
-
-  const tagData: [any] = await got(endpoint, {
-    responseType: "json",
-    resolveBodyOnly: true,
-  });
+  const tagData: [any] = await fetch(endpoint).then((res) => res.json());
   const sorted_responses = tagData.sort(
     (a, b) =>
       new Date(b.published_at).getTime() - new Date(a.published_at).getTime()
@@ -114,51 +108,41 @@ export const downloadBinary = async () => {
     fs.mkdirSync(defaultDownloadDir);
   }
   // now, actually download the thing
-  const downloadOptions = { extract: false, timeout: 10 * 1000 };
-  download(ghReleaseUri, defaultDownloadDir, downloadOptions)
-    .on("downloadProgress", (progress) => {
-      vscode.window.setStatusBarMessage(
-        `mimium: download ${mimiumVersion} from ${ghReleaseUri} \
-              ${(progress.percent * 100).toFixed(1)}% complete`
-      );
-    })
-    .then(
-      // success
-      (value) => {
-        const res = spawnSync("tar", ["-xf", releaseFile], {
-          cwd: defaultDownloadDir,
-        });
-
-        if (res.error) {
-          vscode.window.showErrorMessage(res.error.message);
-        }
-        fs.rmSync(releaseFile, { force: true });
-        fs.chmodSync(
-          path.join(defaultDownloadDir, getExecutableCommand(".")),
-          0o755
-        );
-        // fs.rmSync(localarchive);
-        const sharedir: string = defaultDownloadDir;
-        const config = vscode.workspace.getConfiguration("mimium");
-        config.update("sharedir", sharedir, true);
-        vscode.window.showInformationMessage(
-          `mimium: successfully downloaded \
-                  ${mimiumVersion} to ${sharedir}.\n\
-                  also updating mimium.sharedir config setting`
-        );
-      },
-      // failure
-      (reason) => {
-        vscode.window.showErrorMessage(
-          `mimium: error downloading binary "${reason}"`
-        );
-        return;
-      }
-    )
-    .catch((reason) => {
-      vscode.window.showErrorMessage(
-        `mimium: error installing binary "${reason}"`
-      );
-      return;
+  const downloader = new DownloaderHelper(ghReleaseUri, defaultDownloadDir, {});
+  downloader.on("end", (value) => {
+    const res = spawnSync("tar", ["-xf", releaseFile], {
+      cwd: defaultDownloadDir,
     });
+
+    if (res.error) {
+      vscode.window.showErrorMessage(res.error.message);
+    }
+    fs.rmSync(path.join(defaultDownloadDir, releaseFile), {
+      force: true,
+    });
+    fs.chmodSync(
+      path.join(defaultDownloadDir, getExecutableCommand(".")),
+      0o755
+    );
+    const sharedir: string = defaultDownloadDir;
+    const config = vscode.workspace.getConfiguration("mimium");
+    config.update("sharedir", sharedir, true);
+    vscode.window.showInformationMessage(
+      `mimium: Successfully downloaded \
+              ${mimiumVersion} to ${sharedir}.\n\
+              also updating mimium.sharedir config setting`
+    );
+  });
+  downloader.on("progress", (stats) => {
+    vscode.window.setStatusBarMessage(
+      `mimium: download ${mimiumVersion} from ${ghReleaseUri} \
+            ${stats.progress.toFixed(1)}% complete`
+    );
+  });
+  const error_handler = (e: ErrorStats) => {
+    vscode.window.showErrorMessage(e.message);
+  };
+  downloader.on("error", error_handler);
+
+  downloader.start().catch(error_handler);
 };
