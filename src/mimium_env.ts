e2e971b5c8d53984da1a0a96ccf8c043d32b2534
松浦 知也 Matsuura Tomoya <me@matsuuratomoya.com>
2024-12-17 17:10:43 +0900

    stop opening multiple terminals

--- src/mimium_env.ts
+++ src/mimium_env.ts
@@ -8,6 +8,22 @@ import { spawnSync } from "child_process";
 
 const getConfig = () => vscode.workspace.getConfiguration("mimium");
 
+function getIcons() {
+  const extpath = vscode.extensions.getExtension(
+    "mimium-org.mimium-language"
+  )?.extensionUri;
+  if (extpath) {
+    let dark = vscode.Uri.joinPath(extpath, "mimium_logo_stroke_dark_play.svg");
+    let light = vscode.Uri.joinPath(
+      extpath,
+      "mimium_logo_stroke_light_play.svg"
+    );
+    return { dark: dark, light: light };
+  } else {
+    return undefined;
+  }
+}
+
 export const getMimiumPath = (): string => {
   const config = getConfig();
 
@@ -52,9 +68,6 @@ export const runMimium = (terminal: vscode.Terminal): void => {
   const filepath = editor.document.isUntitled
     ? makeTmpfile(editor, mmmpath)
     : path.basename(editor.document.fileName);
-  if (terminal) {
-    terminal.dispose();
-  }
   const execCommand = getExecutableCommand(mmmpath);
   if (testMimiumExecutable(execCommand) === false) {
     vscode.window.showErrorMessage(
@@ -62,14 +75,20 @@ export const runMimium = (terminal: vscode.Terminal): void => {
           Set mimium.sharedir in the VSCode settings.`
     );
   }
+  let icon = getIcons();
   let shelloption: vscode.TerminalOptions = {
     name: "mimium",
     cwd: path.dirname(editor.document.fileName),
+    iconPath: icon,
   };
   if (platform() === "win32") {
     shelloption.shellPath = vscode.workspace
       .getConfiguration("terminal.external")
       .get("windowsExec");
+  }
+  const mmmterminal = vscode.window.terminals.filter((t) => t.name == "mimium");
+  for (let mt of mmmterminal) {
+    mt.dispose();
   }
   terminal = vscode.window.createTerminal(shelloption);
   terminal.show(true); // show, but don't steal focus
