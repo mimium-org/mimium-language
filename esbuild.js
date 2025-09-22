e23f54b37b268c2d328e4ab7d387e45b61aa23e2
松浦 知也 Matsuura Tomoya <me@matsuuratomoya.com>
2025-09-22 17:01:52 +0900

    moved client files

--- esbuild.js
+++ esbuild.js
@@ -5,7 +5,7 @@ const watch = process.argv.includes('--watch');
 
 async function main() {
   const ctx = await esbuild.context({
-    entryPoints: ['src/extension.ts'],
+    entryPoints: ['client/src/extension.ts'],
     bundle: true,
     format: 'cjs',
     minify: production,
